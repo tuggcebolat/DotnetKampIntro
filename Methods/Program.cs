ae62bf5ecba07689664815db908f3fda8ac90a5f
tuggcebolat <tuggce20@gmail.com>
2023-07-14 01:02:03 +0300

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Methods
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Product urun1 = new Product();
+            urun1.Name = "Apple";
+            urun1.Price = 100;
+            urun1.Description = "Amasya";
+
+            Product urun2 = new Product();
+            urun2.Name = "Waterlemon";
+            urun2.Price = 100;
+            urun2.Description = "Diyarbakır";
+
+            Product[] urunler = new Product[] { urun1, urun2 };
+
+            foreach (Product urun in urunler)
+            {
+
+                Console.WriteLine(urun.Name);
+                Console.WriteLine(urun.Price);  
+                Console.WriteLine(urun.Description);    
+                Console.WriteLine("------------------");    
+
+            }
+
+            Console.WriteLine("-----------Metotlar---------");
+
+            //instance
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Add(urun1);
+            sepetManager.Add(urun2);
+
+
+        }
+    }
+}
---
+++ Methods/SepetManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class SepetManager
+    {
+        public void Add(Product urun)
+        {
+            Console.WriteLine(" Tebrikler sepete eklendi : " + urun.Name);  
+        
+        }   
+    }
+}
