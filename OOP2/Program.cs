70a0810ad12bdc2247e2b3dadd10ac4299d47c31
tuggcebolat <tuggce20@gmail.com>
2023-07-17 15:28:17 +0300

    Updated

---
+++ OOP2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace OOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IndividualCustomer customer1 = new IndividualCustomer();
+            customer1.CustomerNo = "12646468";
+            customer1.FirstName = "Tuğçe";
+            customer1.LastName = "Bolat";
+            customer1.TcNo = "13419879876871";
+
+            CoorporateCustomer customer2 = new CoorporateCustomer();
+            customer2.Id = 2;
+            customer2.CustomerNo = "12886";
+            customer2.CompanyName = "Leon";
+            customer2.TaxId = "2548";
+
+           CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+
+           
+
+           
+        }
+    }
+}
