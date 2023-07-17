baa3de20235d41f537307b81faf59533317f3bfc
tuggcebolat <tuggce20@gmail.com>
2023-07-17 10:54:40 +0300

    end of course

--- GenericsIntro/MyList.cs
+++ GenericsIntro/MyList.cs
@@ -16,8 +16,13 @@ namespace GenericsIntro
         }
         public void Add(T item)
         {
+            T[] tempArray = items;
             items= new T[items.Length+1];
-
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length-1] = item;
         }
         
     }
